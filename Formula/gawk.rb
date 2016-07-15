f55a7f39413e6f280aa894e0fd7b764dcddb46d3
Piotr Gaczkowski <DoomHammerNG@gmail.com>
2016-07-15 13:18:42 +0200

    gawk: skip check if cmp unavailable

--- Formula/gawk.rb
+++ Formula/gawk.rb
@@ -5,6 +5,7 @@ class Gawk < Formula
   sha256 "e3cf55e91e31ea2845f8338bedd91e40671fc30e4d82ea147d220e687abda625"
 
   bottle do
+    revision 2
     sha256 "ed2c5f0b20e4b4af151177a750b1287435aad66bb0c8c4b60daf753f3955332c" => :el_capitan
     sha256 "c6fd269bedf83b9016fb8e09186de1c88ce18f51b1ad471f4b5ad4262066dda2" => :yosemite
     sha256 "30486a1665295a3fee02e22465869cec9297aec262554a470bbc7f127d1766e5" => :mavericks
@@ -25,7 +26,11 @@ class Gawk < Formula
                           "--without-mpfr",
                           "--without-libsigsegv-prefix"
     system "make"
-    system "make", "check"
+    if which "cmp"
+      system "make", "check"
+    else
+      opoo "Skipping `make check` due to unavailable `cmp`"
+    end
     system "make", "install"
   end
 
